90a723977c975854434936214a952b5adacb6a8b
bschwartz10 <bschwartz92@gmail.com>
2017-10-17 19:21:06 -0400

    tests for order

--- test/order_test.rb
+++ test/order_test.rb
@@ -9,9 +9,9 @@ class OrderTest < Minitest::Test
 
   def setup
     @order = Order.new({
-      cash: 12,
+      cash: 14,
       price: 2,
-      wrappers_needed: 5,
+      wrappers_needed: 6,
       type: 'milk'
     })
   end
@@ -24,7 +24,7 @@ class OrderTest < Minitest::Test
   end
 
   def test_order_has_cash
-    assert_equal 12, order.cash
+    assert_equal 14, order.cash
   end
 
   def test_order_has_price
@@ -32,11 +32,40 @@ class OrderTest < Minitest::Test
   end
 
   def test_order_has_wrappers_needed
-    assert_equal 5, order.wrappers_needed
+    assert_equal 6, order.wrappers_needed
   end
 
   def test_order_has_type
     assert_equal 'milk', order.type
+  end
+
+  def order_has_output
+    output = {"milk"=>0, "dark"=>0, "white"=>0, "sugar_free"=>0}
+    assert_equal output, order.output
+  end
+
+  def test_initial_chocolates_bought
+    assert_equal 7, order.initial_chocolates_bought(order.type)
+  end
+
+  # def test_choose_promotion
+  #   assert_equal 0, order.choose_promotion(order.type)
+  # end
+
+  # def test_milk_chocolate_promotion
+  #   output_after_promotion = {"milk"=>8, "dark"=>0, "white"=>0, "sugar_free"=>1}
+  #   assert_equal output_after_promotion, order.chocolate_promotion(order.wrappers_needed, order.initial_chocolates_bought(order.type))
+  # end
+
+  def test_white_chocolate_promotion
+    wc_order = Order.new({
+      cash: 6,
+      price: 2,
+      wrappers_needed: 2,
+      type: 'white'
+    })
+    output_after_promotion = {"milk"=>0, "dark"=>1, "white"=>5, "sugar_free"=>3}
+    assert_equal output_after_promotion, wc_order.chocolate_promotion(wc_order.wrappers_needed, wc_order.initial_chocolates_bought(wc_order.type), wc_order.type)
   end
 
 end
