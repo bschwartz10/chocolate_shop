f65948ea05c5ad3dbd110a243e01baed44b93a56
bschwartz10 <bschwartz92@gmail.com>
2017-10-18 15:48:19 -0400

    create test for orders writer

--- test/orders_writer_test.rb
+++ test/orders_writer_test.rb
@@ -1,6 +1,4 @@
-require './lib/orders_writer'
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper'
 
 class OrdersWriterTest < Minitest::Test
   def test_orders_writer_exists
@@ -8,12 +6,21 @@ class OrdersWriterTest < Minitest::Test
   end
 
   def test_write_orders_method_writes_a_new_csv_file_with_correct_output
-    order = {"milk"=>7, "dark"=>0, "white"=>0, "sugar_free"=>1}
-    OrdersWriter.new(order).write_orders
+    orders = [{'milk'=>7, 'dark'=>0, 'white'=>0, 'sugar free'=>1},
+              {'milk'=>0, 'dark'=>3, 'white'=>0, 'sugar free'=>0},
+              {'milk'=>0, 'dark'=>3, 'white'=>0, 'sugar free'=>5},
+              {'milk'=>0, 'dark'=>1, 'white'=>5, 'sugar free'=>3}]
+    OrdersWriter.new(orders).write_order
     actual_csv = IO.readlines('./output/redemptions.csv')
     first_line = actual_csv[0].strip
-    
+    second_line = actual_csv[1].strip
+    third_line = actual_csv[2].strip
+    fourth_line = actual_csv[3].strip
+
     assert_equal 'milk 7,dark 0,white 0,sugar free 1', first_line
+    assert_equal 'milk 0,dark 3,white 0,sugar free 0', second_line
+    assert_equal 'milk 0,dark 3,white 0,sugar free 5', third_line
+    assert_equal 'milk 0,dark 1,white 5,sugar free 3', fourth_line
   end
 
 end
