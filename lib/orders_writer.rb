3e303cd7e931d4f6f701feec1d8f52a1e7edd4c7
bschwartz10 <bschwartz92@gmail.com>
2017-10-18 16:18:28 -0400

    Narrowed down responsibility to just writing file

--- lib/orders_writer.rb
+++ lib/orders_writer.rb
@@ -1,13 +1,7 @@
 class OrdersWriter
-  attr_reader :all
-
-  def initialize(orders)
-    @all = orders.map {|order| OutgoingOrder.new(order)}
-  end
-
-  def write_order
+  def self.write_order(orders)
     CSV.open('output/redemptions.csv', 'wb') do |csv|
-      all.each do |order|
+      OutgoingOrders.new(orders).all.each do |order|
         csv << ["milk #{order.milk}", "dark #{order.dark}", "white #{order.white}", "sugar free #{order.sugar_free}"]
       end
     end
