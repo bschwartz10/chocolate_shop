462f84aabafa3655b141918394ec1ea228bc0040
bschwartz10 <bschwartz92@gmail.com>
2017-10-18 16:19:29 -0400

    update runner file with self method

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,13 +1,14 @@
 require 'csv'
 require './lib/orders_reader'
-require './lib/orders'
+require './lib/incoming_orders'
 require './lib/incoming_order'
 require './lib/outgoing_order'
 require './lib/orders_writer'
+require './lib/outgoing_orders'
 
 orders_csv = './input/orders.csv'
 orders = OrdersReader.read_orders(orders_csv)
 outputs = orders.all.map do |order|
   order.chocolate_promotion(order.wrappers_needed, order.initial_chocolates_bought(order.type), order.type)
 end
-OrdersWriter.new(outputs).write_order
+OrdersWriter.write_order(outputs)
