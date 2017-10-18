83c9ef0fd9a607b7a45e473ef2c09d84fde9b879
bschwartz10 <bschwartz92@gmail.com>
2017-10-18 15:45:39 -0400

    Cleaned up require statements

--- lib/orders_reader.rb
+++ lib/orders_reader.rb
@@ -1,12 +1,5 @@
-require 'csv'
-require './lib/orders'
-
 class OrdersReader
   def self.read_orders(orders_csv)
     Orders.new(CSV.open orders_csv, headers: true, header_converters: :symbol)
   end
 end
-
-# orders_csv = './input/orders.csv'
-# OrdersReader.read_orders(orders_csv)
-# ''
