798bf1cd806943e77c6d3d9517b41b6fd807c688
bschwartz10 <bschwartz92@gmail.com>
2017-10-27 16:40:27 -0400

    add tests for promotion

---
+++ test/promotion_test.rb
@@ -0,0 +1,45 @@
+require './test/test_helper'
+
+class PromotionTest < Minitest::Test
+  def test_promotion_class_exists
+    assert_kind_of Class, Promotion
+  end
+
+  def test_schedule_method_is_a_hash
+    assert_instance_of Hash, Promotion.schedule
+  end
+
+  def test_has_keys_of_milk_dark_sugar_free_and_white
+    keys = [:milk, :dark, :white, :sugar_free]
+
+    assert_equal Promotion.schedule.keys, keys
+  end
+
+  def test_flavors_have_values_that_are_hashes
+    assert_instance_of Hash, Promotion.schedule[:milk]
+  end
+
+  def test_milk_has_promotion_for_milk_and_sugar_free
+    milk_promotion = { milk: 1, sugar_free: 1}
+
+    assert_equal Promotion.schedule[:milk], milk_promotion
+  end
+
+  def test_dark_has_promotion_for_dark
+    dark_promotion = { dark: 1 }
+
+    assert_equal Promotion.schedule[:dark], dark_promotion
+  end
+
+  def test_white_has_promotion_for_white_and_sugar_free
+    white_promotion = { white: 1, sugar_free: 1 }
+
+    assert_equal Promotion.schedule[:white], white_promotion
+  end
+
+  def test_sugar_free_has_promotion_for_sugar_free_and_dark
+    sugar_free_promotion = { sugar_free: 1, dark: 1 }
+
+    assert_equal Promotion.schedule[:sugar_free], sugar_free_promotion
+  end
+end
