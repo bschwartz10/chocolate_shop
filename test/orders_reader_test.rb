f2896d6ebf484b88399879b71aa89399e07e1780
bschwartz10 <bschwartz92@gmail.com>
2017-10-27 11:51:16 -0400

    break out double assertion into separate tests

--- test/orders_reader_test.rb
+++ test/orders_reader_test.rb
@@ -5,10 +5,15 @@ class OrdersReaderTest < Minitest::Test
     assert_kind_of Class, OrdersReader
   end
 
-  def test_read_orders_method_reads_orders_csv_file_and_creates_order_objects
+  def test_read_orders_method_reads_orders_csv_file_and_returns_array
     orders_csv = './input/orders.csv'
 
     assert_instance_of Array, OrdersReader.read_orders(orders_csv)
+  end
+
+  def test_read_orders_method_returns_an_array_of_orders
+    orders_csv = './input/orders.csv'
+
     assert_instance_of Order, OrdersReader.read_orders(orders_csv).first
   end
 end
