41cf1f6f714dd890b2b10c75a7fe5deedfe19241
bschwartz10 <bschwartz92@gmail.com>
2017-10-30 12:13:23 -0400

    add comments for readability

--- lib/order.rb
+++ lib/order.rb
@@ -11,6 +11,7 @@ class Order
   end
 
   def process!
+    # execute promotion
     bought_chocolates = initial_chocolates_bought
     initial_wrappers_obtained(bought_chocolates)
     chocolate_promotion
@@ -27,6 +28,7 @@ private
   end
 
   def chocolate_promotion
+    # execute run_promotion until all eligible wrappers are traded in
     loop do
       free_chocolates = available_wrappers.select do |flavor, wrappers|
         wrappers >= wrappers_needed
@@ -36,6 +38,7 @@ private
   end
 
   def run_promotion(free_chocolates)
+    # trade in eligible wrappers and redeem free chocolates
     free_chocolates.each do |flavor, wrappers|
       available_wrappers[flavor] -= wrappers_needed
       add_chocolates(flavor)
@@ -43,6 +46,7 @@ private
   end
 
   def add_chocolates(flavor)
+    # retrieve correct chocolate redemptions and increase corresponding wrappers
     Promotion.rules[flavor].each do |type, amount|
       redemptions[type] += amount
       available_wrappers[type] += amount
