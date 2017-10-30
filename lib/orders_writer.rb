f522b3fb8b3b9a8da7c6d42c4d128d789254fe1f
bschwartz10 <bschwartz92@gmail.com>
2017-10-30 12:13:36 -0400

    make consistent with single quotes

--- lib/orders_writer.rb
+++ lib/orders_writer.rb
@@ -2,7 +2,7 @@ class OrdersWriter
   def self.export(orders)
     CSV.open('output/redemptions.csv', 'wb') do |csv|
       orders.each do |order|
-        csv << order.redemptions.map {|flavor, quantity| "#{flavor.to_s.gsub("_", " ")} #{quantity}"}
+        csv << order.redemptions.map {|flavor, quantity| "#{flavor.to_s.gsub('_', ' ')} #{quantity}"}
       end
     end
   end
